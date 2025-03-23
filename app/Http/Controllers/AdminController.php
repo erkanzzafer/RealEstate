d66e7d880747b36d41d2af645af773d4b614a9c6
Zafer Erkan <83276774+erkanzzafer@users.noreply.github.com>
2025-03-23 15:27:45 +0300

    admin profil güncelleme sayfası tamamlandı

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
 
@@ -29,5 +30,37 @@ class AdminController extends Controller
         return view('admin.admin_login');
 
     }
+
+
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }
+
+    public function AdminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        $data->username = $request->username;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+        return redirect()->back();
+
+    }
+
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -33,12 +33,16 @@ Route::middleware('auth')->group(function () {
 
 
 
+//Admin Group Middleware
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 });
 
 
+//Agent Group Middleware
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
